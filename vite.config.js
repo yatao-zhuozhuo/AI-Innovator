3c195a33257d78cba2c6860e9eeae2a90b9be098
yatao-zhuozhuo <3229921494@qq.com>
2026-03-29 09:45:19 +0800

    configure for docker deployment with aliyun backend

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 1. 添加 base 路径，填写你的 GitHub 仓库名
-  base: '/AI-Innovator/', 
-  
+
   server: {
     host: '0.0.0.0',
     port: 3000,
